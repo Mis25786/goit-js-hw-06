c9fd1853162cb081988138b02b3ccd7680cdc6b2
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-01-08 13:57:23 +0100

    hw-6_2

--- js/task-02.js
+++ js/task-02.js
@@ -14,3 +14,24 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+// const newArrayEl = ingredients.forEach((element) => {
+//   const newEl = document.createElement("li");
+//   newEl.classList.add("item");
+//   newEl.textContent = element;
+// });
+// console.log(newArrayEl);
+
+/////////////
+const newEl = document.createElement("li");
+// console.log(newEl);
+
+newEl.classList.add("item");
+// console.log(newEl);
+
+newEl.textContent = ingredients[i];
+console.log(newEl);
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  console.table((newEl.textContent = ingredients[i]));
+}
