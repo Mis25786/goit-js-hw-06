95b245244b6eefad2f80cb8a53003feae93f5e80
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-01-16 01:16:26 +0100

    hw-6_9

--- js/task-08.js
+++ js/task-08.js
@@ -14,7 +14,72 @@
 //* </form>
 
 //todo Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+
 //todo Під час відправлення форми сторінка не повинна перезавантажуватися.
-//todo Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-//todo Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+
+//todo Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+//todo що всі поля повинні бути заповнені.
+
+//todo Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт,
+//todo де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості.
+//todo Для доступу до елементів форми використовуй властивість elements.
+
 //todo Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const form = document.querySelector(".login-form");
+// console.log(form);
+
+form.addEventListener("submit", onSubmitForm);
+// console.dir(form);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  //   console.dir(event.currentTarget);
+
+  const formEl = event.currentTarget.elements;
+  //   console.log(formEl);
+
+  if (formEl.email.value === "" || formEl.password.value === "") {
+    alert("Заповніть будь-ласка всі поля");
+  }
+  //*============================================
+  //   if (formEl.email.value === "") {
+  //     alert("Заповніть будь-ласка всі поля");
+  //   } else if (formEl.password.value === "") {
+  //     alert("Заповніть будь-ласка всі поля");
+  //   }
+  //*============================================
+  //     formEl.email.value == "" && formEl.name.value == ""
+  //       ? "Заповніть будь-ласка всі поля"
+  //       : що потрібно тут? ;
+
+  //* задається => typ
+  const email = formEl.email.value;
+  //   console.log(email);
+  const password = formEl.password.value;
+  //   console.log(password);
+
+  const formData = {
+    email,
+    password,
+  };
+
+  //* виводимо об'єкт з данними в консоль
+  console.log(formData);
+
+  //* очистити всі поля form
+  form.reset();
+}
+
+//?======================================
+
+//* збір данних з великих полів
+//* ===========================
+//   const formData = new formData(event.currentTarget);
+
+//   formData.forEach((value, name) => {
+//     console.log(name, value);
+//   });
+
+//   console.log(formData);
+// }
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,47 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//! Завдання 9
+//todo Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку
+//todo на button.change-color і виводить значення кольору в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+//*=======================================================
+
+const button = document.querySelector(".change-color");
+console.log(button);
+
+button.addEventListener("clic", changeColor);
+
+const spanColorText = document.querySelector(".color");
+// console.log(spanColorText);
+
+function changeColor(event) {
+  event.preventDefault();
+  console.log("object");
+  console.log(event.currentTarget);
+
+  // const backgraundColorBody = document.body.style.backgroundColor;
+  // console.dir(backgraundColorBody);
+
+  //=========================================
+  // currentTarget = getRandomHexColor();
+  //==========================================
+  // function getRandomHexColor() {
+  //   return (backgraundColorBody = `#${Math.floor(
+  //     Math.random() * 16777215
+  //   ).toString(16)}`);
+  // }
+  //=========================================
+
+  // spanColorText.textContent = backgraundColorBody;
 }
